fcdec566ac6b629cc0d25393e53d29289b13e51c
Riccardo Giomi <me.personally.myself@gmail.com>
2024-03-05 16:46:30 +0100

    Recent fixes to the quickstart template applied.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,20 +18,20 @@ module ApplicationHelper
   def model_date_value(model, attribute_name, default: '---')
     value = model.try(attribute_name)
 
-    value.present ? I18n.l(value, format: :template_date) : default
+    value.present? ? I18n.l(value, format: :template_date) : default
   end
 
   def model_datetime_value(model, attribute_name, default: '---')
     value = model.try(attribute_name)
 
-    value.present ? I18n.l(value, format: :template_datetime) : default
+    value.present? ? I18n.l(value, format: :template_datetime) : default
   end
   alias model_timestamp_value model_datetime_value
 
   def model_time_value(model, attribute_name, default: '---')
     value = model.try(attribute_name)
 
-    value.present ? I18n.l(value, format: :template_time) : default
+    value.present? ? I18n.l(value, format: :template_time) : default
   end
 
   def default_image_tag(image, variant: nil)
